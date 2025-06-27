c7d69cad6ecc9b001fbf339f51641cc3226aa9eb
Girish Garg <garggirish2020@gmail.com>
2025-06-27 19:52:02 +0530

    started the work

---
+++ components/custom/logo.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
+import { cn } from '@/lib/utils'
+
+const Logo = ({className}) => {
+  return (
+    <div className={cn("flex w-24 h-16 -space-x-8", className)}>
+        <Avatar className="z-10 size-16">
+            <AvatarImage src="/MBM.svg" alt="MBM" />
+            <AvatarFallback>MBM</AvatarFallback>
+        </Avatar>
+        <Avatar className="z-0 size-16">
+            <AvatarImage src="/apk.svg" alt="Ment Hub" />
+            <AvatarFallback>MH</AvatarFallback>
+        </Avatar>
+    </div>
+  )
+}
+
+export default Logo
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Logo from '@/components/custom/logo'
+function App() {
+  return (
+    <>
+      <Logo />
+    </>
+  )
+}
+
+export default App
