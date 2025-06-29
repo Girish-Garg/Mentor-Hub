08fecc6484cea0aa890407f86087c152dcc65e16
Sapna Pohwani <141144857+SapnaPohwani@users.noreply.github.com>
2025-06-29 12:05:28 +0530

    QuestionComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Badge from '@/components/custom/Badge'
-import './App.css'
 import Logo from '@/components/custom/logo'
 import Resource from '@/components/custom/Resource'
+import Question from '@/components/custom/Question'
 function App() {
   return (
     <>
@@ -9,6 +9,13 @@ function App() {
       <Badge text="Votes" varient="filter"/>
       <br />
       <Resource title='Math 1 Notes' author='Girish Garg' uploadDate='12/11/2025' tags={['Student','Math 1', 'Civil Sem', 'Notes']}/>
+      <Question
+        title="How Should i prepare for my maths 2 Examination of 2nd sem ..?"
+        description="I'm a 2nd-semester student and have my Maths 2 exam coming up. I want to know the most effective way to prepare for it. The syllabus include topic ike differential equations, Laplace transforms, vector calculus, and multiple integrals. I’m looking for a solid preparation strsdfsdsdvsdvsdv sdvsdvsdvsdvs...."
+        author='Girish Garg'
+        uploadDate="12/11/2025"
+        tags={['Student', 'Maths 2', 'Mech sem']}
+      />
     </>
   )
 }
