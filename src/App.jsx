3ef885260daa2420df661a466ef9eeb1a69fd24e
Sapna Pohwani <141144857+SapnaPohwani@users.noreply.github.com>
2025-06-29 20:44:36 +0530

    Event-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,36 @@
-import Badge from '@/components/custom/Badge'
-import Logo from '@/components/custom/logo'
-import Resource from '@/components/custom/Resource'
-import Question from '@/components/custom/Question'
-import Login from '@/src/pages/auth/Login'
-import AccType from '@/components/SignUp/AccType'
+import Badge from "@/components/custom/Badge";
+import Logo from "@/components/custom/logo";
+import Resource from "@/components/custom/Resource";
+import Question from "@/components/custom/Question";
+import Events from "@/components/custom/Event";
 function App() {
   return (
     <>
-      {/* <Logo />
-      <Badge text="Votes" varient="filter"/>
+      <Logo />
+      <Badge text="Votes" varient="filter" />
       <br />
-      <Resource title='Math 1 Notes' author='Girish Garg' uploadDate='12/11/2025' tags={['Student','Math 1', 'Civil Sem', 'Notes']}/>
-      <Resource title='Math 1 Notes' author='Girish Garg' uploadDate='12/11/2025' tags={['Student','Math 1', 'Civil Sem', 'Notes']} initialUserVote='down' initialVotes={-1}/> */}
-    {/*<Login/>*/}
-      {/* <AccType/> */}
+      <Resource
+        title="Math 1 Notes"
+        author="Girish Garg"
+        uploadDate="12/11/2025"
+        tags={["Student", "Math 1", "Civil Sem", "Notes"]}
+      />
       <Question
         title="How Should i prepare for my maths 2 Examination of 2nd sem ..?"
         description="I'm a 2nd-semester student and have my Maths 2 exam coming up. I want to know the most effective way to prepare for it. The syllabus include topic ike differential equations, Laplace transforms, vector calculus, and multiple integrals. I’m looking for a solid preparation strsdfsdsdvsdvsdv sdvsdvsdvsdvs...."
-        author='Girish Garg'
+        author="Girish Garg"
         uploadDate="12/11/2025"
-        tags={['Student', 'Maths 2', 'Mech sem']}
+        tags={["Student", "Maths 2", "Mech sem"]}
+      />
+      <Events
+        title="Encarta the MBM event..."
+        description="With the commencement of the 2025 academic session, MBM University proudly presents Encarta, its flagship annual event. Encarta is more than just a fest—it's a grand celebration of innovation, creativity, and cultural vibrance. Held every year,Encarta is a celebration of student energy, creativity and..."
+        uploadTime="1 min ago"
+        uploadDate="24 Jun’25  - 28 Jun’25"
+        tags={["College", "Fest", "Students"]}
       />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
