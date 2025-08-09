da9bebffb116d19aa58e1e1f7b69169543470d2f
Girish Garg <garggirish2020@gmail.com>
2025-08-09 17:46:52 +0530

    did changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,10 @@
-
 import React from 'react'
-import HomePage from './Questionare_Components/HomePage'
-import AskQuestion from '../components/Overlay_Components/AddAQuestion'
-// import { TEMP_FILTER_CONFIG } from "../src/test";
-import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
-import AskResource from '../components/Overlay_Components/AddAAnswer';
-import AddResource from '../components/Overlay_Components/AddAResource';
-import AddAnswer from '../components/Overlay_Components/AddAAnswer';
-import AddQuestion from '../components/Overlay_Components/AddAQuestion';
-import Tag from '../components/Overlay_Components/Tag';
-import EventPage from '@/src/Questionare_Components/EventPage';
-import ResourcePage from './Questionare_Components/ResourcePage';
-const App = () => {
-  return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-    <ResourcePage />
-        </MantineProvider>
-  )
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
 } from "react-router-dom";
-import Badge from "@/components/custom/Badge";
-import Logo from "@/components/custom/logo";
-import Resource from "@/components/custom/Resource";
-import Question from "@/components/custom/Question";
 import Login from "@/src/pages/auth/Login";
 import AccType from "./pages/auth/SignUp/AccType";
 import CreateAcc from "./pages/auth/SignUp/CreateAcc";
